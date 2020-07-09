b75dcb2ff67113843bc49be7de44e4aa44129dc8
Lukas Ramonas <hq019153@live.reading.ac.uk>
2020-07-09 15:41:22 +0100

    spring initializer

---
+++ cms/src/main/java/com/lukas/ramonas/cms/ConferenceManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lukas.ramonas.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceManagmentSystemApplication.class, args);
+	}
+
+}
