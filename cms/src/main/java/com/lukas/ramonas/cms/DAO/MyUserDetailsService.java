55cb1fd5ebc4e376c8f3d89a19e4be1c398270b4
Lukas Ramonas <hq019153@live.reading.ac.uk>
2020-10-06 21:28:53 +0100

    All users in database now get authorized and let through the login page although does not have authorities/role functionality as role.java throws error.

---
+++ cms/src/main/java/com/lukas/ramonas/cms/DAO/MyUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.lukas.ramonas.cms.DAO;
+
+import com.lukas.ramonas.cms.Principal.MyUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.lukas.ramonas.cms.Model.User;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)  throws UsernameNotFoundException{
+       // BCryptPasswordEncoder encoder = passwordEncoder();
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new MyUserDetails(user);
+    }
+
+    
+
+}
---
+++ cms/src/main/java/com/lukas/ramonas/cms/Model/User.java
@@ -0,0 +1,88 @@
+package com.lukas.ramonas.cms.Model;
+
+import org.hibernate.annotations.Cascade;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+
+/*******************************************
+ * Defined entity model
+ *******************************************/
+@Entity(name = "User")
+@Table(name = "user_table")
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer user_id;
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String password;
+
+    private String email;
+
+//    @OneToMany(orphanRemoval=true,  mappedBy="User", fetch = FetchType.EAGER)
+//    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+//    private List<Role> roles;
+
+    private boolean confirmed;
+
+    public Integer getId() {
+        return user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return this.roles;
+//    }
+
+//    public void setAuthorities(String authorities) {
+//        this.roles = authorities;
+//    }
+
+    public Boolean getConfirmed() {
+        return confirmed;
+    }
+
+    public void setConfirmed(Boolean confirmed) {
+        this.confirmed = confirmed;
+    }
+
+
+}
