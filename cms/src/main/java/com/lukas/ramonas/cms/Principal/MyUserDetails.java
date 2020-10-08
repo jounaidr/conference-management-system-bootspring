e82866852cb76c1dd9719122dc0b24230133855a
MexMass <ramonas.lukas@gmail.com>
2020-10-08 16:56:35 +0100

    test to see if github recognizes my commits
    Signed-off-by: MexMass <ramonas.lukas@gmail.com>

--- cms/src/main/java/com/lukas/ramonas/cms/Principal/MyUserDetails.java
+++ cms/src/main/java/com/lukas/ramonas/cms/Principal/MyUserDetails.java
@@ -21,7 +21,6 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-
         return null;
 //        return user.getAuthorities();
     }
